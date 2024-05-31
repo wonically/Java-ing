67ad80a6ddb7d57c37d2146b15bd175d12db3818
Wony Nguyen <nhd.wyd.2005@gmail.com>
2024-05-31 11:54:10 +0700

    let's study together

--- src/Array_ArrayList.java
+++ src/Array_ArrayList.java
@@ -1,3 +1,17 @@
+import java.util.Arrays;
+
 public class Array_ArrayList {
-    /*
+
+    public static void main(String[] args) {
+        //Declaring an array
+        //int[] myFavNumbers = new int[5]; or
+        int[] myFavNumbers = {0, 2, 4, 6, 8};
+
+        // Arrays don't support modifying a single element, so the following will yield an error:
+        // myFavNumbers[0] = 1;
+        // If changed, then change all.
+
+        System.out.println("My favorite numbers are: " + Arrays.toString(myFavNumbers));
+        System.out.println("My smallest favorite numbers are: " + myFavNumbers[0]);
+    }
 }
