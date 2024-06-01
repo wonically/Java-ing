b40a27b2e4c4386e75530e4a127524d259abfada
Wony Nguyen <nhd.wyd.2005@gmail.com>
2024-06-01 09:45:22 +0700

    Create Wrapper.java

---
+++ src/Wrapper.java
@@ -0,0 +1,25 @@
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+
+public class Wrapper {
+    public static void main(String[] args) {
+        //Use primitive data types as Objects.
+
+        /*
+        byte	Byte
+        short	Short
+        int     Integer
+        long	Long
+        float	Float
+        double	Double
+        boolean	Boolean
+        char	Character
+         */
+
+        //To take value, use .<type>Value() method.
+        Integer i = 2;
+        //i = i.intValue(); converts it to int.
+        //int n = i; this also does the trick.
+        System.out.println(i);
+    }
+}
